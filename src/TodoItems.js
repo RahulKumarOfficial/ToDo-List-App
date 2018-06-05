93dd104bf71d4b3969c5a66164b4c334bcc540ec
Rahul Kumar <rahulkr9622@gmail.com>
2018-06-05 14:20:50 +0530

    Update TodoItems.js

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -1,33 +1,4 @@
-<<<<<<< HEAD
-import React, { Component } from "react";
-class TodoItems extends Component {
-  constructor(props) {
-    super(props);
-    this.deleteItem = this.deleteItem.bind(this);
-    this.createTasks = this.createTasks.bind(this);
-  }
-  createTasks(items) {
-    return (
-      <li>
-        {items.text}
-        <button type="submit" onClick={this.deleteItems}>
-          Delete
-        </button>
-      </li>
-    );
-  }
-  deleteItem(key){
-      this.props.deleteItem(key);
-      console.log("passed to todoItems.js");
-  }
-  render() {
-    var TodoEntries = this.props.entries;
-    var listItems = TodoEntries.map(this.createTasks);
-    return <ul>{listItems}</ul>;
-  }
-}
-export default TodoItems;
-=======
+
 import React,{Component} from "react";
 import uniqueId from 'react-html-id';
 class TodoItems extends Component{
@@ -92,4 +63,4 @@ delete(text){
     }
 }
 export default TodoItems;
->>>>>>> rahul
+
