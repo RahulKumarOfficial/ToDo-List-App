92880868e5356fb31301699fb9b785c8bec705b3
rahulkumar082 <rahulkr9622@gmail.com>
2018-06-05 14:07:57 +0530

    Buggy Update functionality

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -1,44 +1,59 @@
 import React,{Component} from "react";
-
+import uniqueId from 'react-html-id';
 class TodoItems extends Component{
     constructor(props){
         super(props);
-
         this.createTasks= this.createTasks.bind(this);
-
+        uniqueId.enableUniqueIds(this);
+       
      }
 
     handleChange(text){
         console.log("Editable mode!");
     }
+
     updateField(event){
        // console.log(event.target.value);
-       
-        this.setState({text:event.target.value});
-        var text = event.target.value;
-        console.log(text);
-        
-    }
-    onChange = (event) =>{
-        this.setState({text:event.target.value})
-        console.log(event.target.value);
+
+        // this.setState({text:event.target.value});
+        // var text = event.target.value;
+        // console.log(text);
+
+
     }
 
+changeIt(event){
+    //console.log("Id generated is :" + id);
+
+    this.setState({
+        
+        inputValue : event 
+    });
+   console.log(event.target.value);
+}   
+
     createTasks(item){
-        return <div id="one"><center><li><input value={item.text} onChange={this.onChange}/>
+        this.state={
+            inputValue:item.text
+        }
+        console.log(item.text + " written inside createTasks");
+        return <div id="one"><center><li><input value={this.state.inputValue} onChange ={this.changeIt.bind(this)}/>       
         <button type="submit" onClick={()=>this.delete(item.text)}>Delete</button>
+        <buton type = "button" >Update</buton>
         </li></center>
         </div>
-        
+
 }
 delete(text){
     this.props.delete(text);
-    
+
 }
 
     render(){
         var todoEntries = this.props.entries;
         var listItems = todoEntries.map(this.createTasks);
+        var text = this.props.text;
+     
         return(
             <ul>
             {listItems}
